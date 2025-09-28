3cd2d4501eaa511f79112d71e9fe49141624b155
JulianeMaran32 <julianemaran@gmail.com>
2025-09-28 14:41:40 -0300

    refactor: Adiciona classes DTO para mapear dados de entrada e saída

--- user-service/src/main/java/com/identityaccessdomain/userservice/dto/UserResponseDTO.java
+++ user-service/src/main/java/com/identityaccessdomain/userservice/dto/UserResponseDTO.java
@@ -1,9 +1,18 @@
 package com.identityaccessdomain.userservice.dto;
 
+import lombok.Builder;
+
 /**
  * user-service
+ *
  * @author Juliane Maran
  * @since 28/09/2025
  */
-public record UserResponseDTO() {
+@Builder
+public record UserResponseDTO(
+  Long id,
+  String firstName,
+  String lastName,
+  String email
+) {
 }
