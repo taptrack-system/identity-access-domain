766aea1cd92d76f2119931350859e5ab50e0fef9
JulianeMaran32 <julianemaran@gmail.com>
2025-09-28 14:42:24 -0300

    refactor: Adiciona interface para mapear a conversão entre entidades e dtos

--- user-service/src/main/java/com/identityaccessdomain/userservice/mapping/UserMapper.java
+++ user-service/src/main/java/com/identityaccessdomain/userservice/mapping/UserMapper.java
@@ -1,9 +1,21 @@
 package com.identityaccessdomain.userservice.mapping;
 
+import com.identityaccessdomain.userservice.dto.UserRequestDTO;
+import com.identityaccessdomain.userservice.dto.UserResponseDTO;
+import com.identityaccessdomain.userservice.model.User;
+import org.mapstruct.Mapper;
+
 /**
  * user-service
+ *
  * @author Juliane Maran
  * @since 28/09/2025
  */
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+
+  User requestDtoToEntity(UserRequestDTO requestDTO);
+
+  UserResponseDTO entityToResponseDto(User user);
+
 }
