7deb044b92ea05bf14b26ac1aae9fb9055b8cb66
JulianeMaran32 <julianemaran@gmail.com>
2025-09-28 14:44:51 -0300

    refactor: atualiza o controller e o model para evitar expor diretamente a entidade

--- user-service/src/main/java/com/identityaccessdomain/userservice/controller/UserController.java
+++ user-service/src/main/java/com/identityaccessdomain/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.identityaccessdomain.userservice.controller;
 
-import com.identityaccessdomain.userservice.model.User;
+import com.identityaccessdomain.userservice.dto.UserResponseDTO;
+import com.identityaccessdomain.userservice.mapping.UserMapper;
 import com.identityaccessdomain.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * user-service
@@ -24,22 +26,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
+  private final UserMapper userMapper;
   private final UserService userService;
 
   @GetMapping
-  public ResponseEntity<List<User>> listUsers() {
+  public ResponseEntity<List<UserResponseDTO>> listUsers() {
     log.info("Listando todos os usuários");
-    return ResponseEntity.ok(userService.findAllUsers());
+    List<UserResponseDTO> users = userService.findAllUsers()
+      .stream()
+      .map(userMapper::entityToResponseDto)
+      .collect(Collectors.toList());
+    return ResponseEntity.ok(users);
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<User> getById(@PathVariable Long id) {
+  public ResponseEntity<UserResponseDTO> getById(@PathVariable Long id) {
     log.info("Buscando usuário com ID {}", id);
     return userService.findUserById(id)
-      .map(user -> {
-        log.debug("Usuário encontrado: {}", user);
-        return ResponseEntity.ok(user);
-      })
+      .map(userMapper::entityToResponseDto)
+      .map(ResponseEntity::ok)
       .orElseGet(() -> {
         log.warn("Usuário com ID {} não encontrado", id);
         return ResponseEntity.notFound().build();
--- user-service/src/main/java/com/identityaccessdomain/userservice/model/User.java
+++ user-service/src/main/java/com/identityaccessdomain/userservice/model/User.java
@@ -26,20 +26,20 @@ public class User {
   private Long id;
 
   @NotBlank(message = "O primeiro nome é obrigatório.")
-  @Size(min = 2, max = 50, message = "O primeiro nome deve ter entre 2 e 50 caracteres.")
+  @Size(min = 3, max = 50, message = "O primeiro nome deve ter entre {min} e {max} caracteres.")
   @Column(name = "first_name", nullable = false, length = 50)
   private String firstName;
 
   @NotBlank(message = "O sobrenome é obrigatório.")
-  @Size(min = 2, max = 50, message = "O sobrenome deve ter entre 2 e 50 caracteres.")
-  @Column(name = "last_name", nullable = false, length = 50)
+  @Size(min = 3, max = 50, message = "O sobrenome deve ter entre {min} e {max} caracteres.")
+  @Column(name = "last_name", nullable = false, length = 150)
   private String lastName;
 
 
   @NotBlank(message = "O e-mail é obrigatório.")
   @Email(message = "O e-mail deve ser válido.")
-  @Size(max = 100, message = "O e-mail deve ter no máximo 100 caracteres.")
-  @Column(name = "email", unique = true, nullable = false, length = 100)
+  @Size(max = 150, message = "O e-mail deve ter no máximo {max} caracteres.")
+  @Column(name = "email", unique = true, nullable = false, length = 150)
   private String email;
 
 }
