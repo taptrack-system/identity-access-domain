a55ff899443831932060cb867e1f8dd7b9abbb1e
JulianeMaran32 <julianemaran@gmail.com>
2025-10-02 20:57:05 -0300

    Deleta arquivos

---
+++ identity-profiles/src/main/java/com/identityaccessdomain/identityprofiles/IdentityProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.identityaccessdomain.identityprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityProfilesApplication.class, args);
+	}
+
+}
