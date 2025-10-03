dd5b0d8318a4cb69aec80932b4614108fece36b1
JulianeMaran32 <julianemaran@gmail.com>
2025-10-03 16:22:07 -0300

    feature: adiciona exceções personalizadas

---
+++ identity-profiles/src/main/java/com/identityaccessdomain/identityprofiles/exception/ConflictException.java
@@ -0,0 +1,15 @@
+package com.identityaccessdomain.identityprofiles.exception;
+
+/**
+ * identity-profiles
+ *
+ * @author Juliane Maran
+ * @since 03/10/2025
+ */
+public class ConflictException extends RuntimeException {
+
+  public ConflictException(String message) {
+    super(message);
+  }
+
+}
